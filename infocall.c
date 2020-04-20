ba1c5330994bcb7654c84bb2ef98a6e9124fbffd
Longze SU <lsu018@ucr.edu>
2020-04-19 23:46:30 -0700

    info

--- infocall.c
+++ infocall.c
@@ -6,9 +6,9 @@ int
 main(int argc, char *argv[])
 {
     printf(0, "LAB1 STARTING ...\n");
-    printf(0, "Process Numbers are: %d\n", lab1(1)); 
-    printf(0, "SYSCALL Numbers are: %d\n", lab1(2)); 
-    printf(0, "Memory Page Numbers are: %d\n", lab1(3));
+    printf(0, "Process Numbers are: %d\n", info(1)); 
+    printf(0, "SYSCALL Numbers are: %d\n", info(2)); 
+    printf(0, "Memory Page Numbers are: %d\n", info(3));
     printf(0, "LAB1 END\n");
 
     exit();
